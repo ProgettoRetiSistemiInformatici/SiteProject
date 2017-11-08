a06ead2adb19f1e360e5c3fee8d52cc69fc97a09
Gianz91 <gianlucabianchini91@gmail.com>
2017-11-08 11:56:25 +0100

    Update registrazione.php

--- registrazione.php
+++ registrazione.php
@@ -1,15 +1,13 @@
 <?php
-
+//include('MySession.php');
 include ('dbconnection.php');
-
 session_start();
+
 
 $username = $_POST['username'];
 $password = $_POST['password'];
 $email = $_POST['email'];
-
-$_SESSION["utente"] = $username;
-
+$_SESSION['utente'] = $username;
 $username = filter_var($username, FILTER_SANITIZE_STRING);
 $password = filter_var($password, FILTER_SANITIZE_STRING);
 $email = filter_var($email, FILTER_SANITIZE_EMAIL);
@@ -20,14 +18,17 @@ if(!$username || !$password || !$email){
 
 // Connessione al database
 global $mysqli;
-$password = hash('sha256', $password); //Creazione dell'hash
-$query = "INSERT INTO login (user, password, email) VALUES ('$username', '$password', '$email')";
+
+$password = hash('sha256', $password);//Creazione dell'hash
+$mysqli-> real_escape_string($username);
+$mysqli-> real_escape_string($password);
+$query = "INSERT INTO login (user, password, email) VALUES ('$username', '$password', '$email');";
 // Esecuzione della query e controllo degli eventuali errori
 if (!$mysqli->query($query)) {
 	die($mysqli->error);
 }
 $mysqli->close();
-
+session_write_close();
 ?>
 <!DOCTYPE html>
 <html>
@@ -38,7 +39,7 @@ $mysqli->close();
 	<h1>Risultati registrazione</h1>
 	<?php if ($error): ?>
 		<p style="color: red"><?php echo $error ?></p>
-	<?php else: header('Location: /home.php?user='.$_SESSION["utente"]); ?>		
+	<?php else: header('Location: /home.php?user='.$username); ?>		
 	<?php endif ?>
 </body>
 </html>
