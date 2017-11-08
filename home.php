b0020e36d60d1c11105cccde0842d53491585ea6
Gianz91 <gianlucabianchini91@gmail.com>
2017-11-08 11:53:45 +0100

    Update home.php

--- home.php
+++ home.php
@@ -9,12 +9,11 @@ if(!isset($_SESSION["utente"])){
     header("Location: /index.php");
 }
 
-global $mysqli;
-$query = "SELECT name, description FROM photo ORDER BY id LIMIT 6;";
+//global $mysqli;
+$query = "SELECT name, description FROM photo ORDER BY id LIMIT 50;";
 if (!$result = $mysqli->query($query)){
      echo "Errore nella query";
 }
-
 $mysqli->close();
 ?>
 
@@ -23,9 +22,11 @@ $mysqli->close();
 <head>
 <title>Home</title>
 <meta charset="UTF-8">
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 <style>
 ul {
+    position: -webkit-sticky;
+    position: sticky;
     list-style-type: none;
     margin: 0;
     padding: 0;
@@ -64,6 +65,14 @@ div.desc {
 </style>
 </head>
 <body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/it_IT/sdk.js#xfbml=1&version=v2.10&appId=124840911516975';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 
 <header>
   <h1><b>PHOTOLIO</b></h1>
@@ -74,19 +83,23 @@ div.desc {
 <ul>
   <li><a href="<?php echo "/home.php?user=" .$_SESSION["utente"] ?>" >Home</a></li>
   <li><a href="uploadFile.html">Load Image</a></li>
-  <li><a href="#contact">Share Us</a></li>
+  <li><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fphotolio.com%2F&amp;src=sdkpreparse">Share Us</a></li>
   <li><a href="logOut.php">Log Out</a></li>
 </ul>
 
 <!-- Photo Grid -->
-
 <div><?php /*Fetch object array */
     while($obj = $result->fetch_object()){ ?>
       <div class="gallery">
-        <a target="_blank" href="<?php echo $obj->name ?>">
+        <a href="/comments.php?photo=<?php echo $obj->name?>"> 
           <img src="<?php echo "/uploads/".$obj->name ?>" alt="Immagine" width="300" height="200">
         </a>
-        <div class="desc"> <?php echo $obj->description ?> </div>
+        <div class="desc"> <?php echo $obj->description ?> | 
+            <div class="fb-share-button" data-href="http://photolio.com/fotopage.php?photo="<?php echo $obj->name ?>
+                    data-layout="button_count" data-size="small" data-mobile-iframe="true">
+        		<a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Ftest.com%2Fcomments.php%3Fphoto%3D<?php echo $obj->name?>&amp;src=sdkpreparse">Condividi</a>
+        	</div>
+        </div>
       </div>
     <?php } ?>
 </div>
