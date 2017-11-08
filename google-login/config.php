ee5089ebea52ab3ee0d57f7721d477edd12f2da1
Gianz91 <gianlucabianchini91@gmail.com>
2017-11-08 11:31:35 +0100

    Update config.php

--- google-login/config.php
+++ google-login/config.php
@@ -2,9 +2,9 @@
 	session_start();
 	require_once "GoogleAPI/vendor/autoload.php";
 	$gClient = new Google_Client();
-	$gClient->setClientId("940202534728-r2pmss81add0n9c5mpf4q4ip2bsca96n.apps.googleusercontent.com");
-	$gClient->setClientSecret("G-ueTo6FANA4i-0oo-bsP0af");
-	$gClient->setApplicationName("PRSI-Google-Oauth");
-	$gClient->setRedirectUri("http://localhost/google-login/g-callback.php");
+	$gClient->setClientId("YOUR_CLIENT_ID");
+	$gClient->setClientSecret("YOUR_CLIENT_SECRET");
+	$gClient->setApplicationName("YOUR_APPLICATION_NAME");
+	$gClient->setRedirectUri("YOUR_REDIRECT_URI");
 	$gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
 ?>
