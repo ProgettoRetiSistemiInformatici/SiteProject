c507492d9c5e9b0c263783c4423f088fa4ddacd1
Gianz91 <gianlucabianchini91@gmail.com>
2017-11-08 11:55:26 +0100

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,88 +1,93 @@
 <?php
 //include ('MySession.php');
-
 include('dbconnection.php');
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 session_start();
 
+global $mysqli;
 global $name;
 global $target_file;
 global $error;
 global $uploadOk;
-$fname = $_POST["newName"];
+
+$MAXDIM = 10000000;
 $target_dir = "uploads/";
-if ($fname == null) {
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$name = basename($_FILES["fileToUpload"]["name"]);
-}
-else {
-$target_file = $target_dir . $fname;
-$name = $fname;
-}
+$name = $_FILES["fileToUpload"]["name"];
+$target_file = $target_dir . basename($name);
+$tmpName = $_FILES["fileToUpload"]["tmp_name"];
 $uploadOk = 1;
 
-if($_FILES['fileToUpload']['error'] > 0){
+if($_FILES['fileToUpload']['error'] !== 0){
     die('An error ocurred when uploading.');
 }
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-//non permette l'Upload di alcuni tipi di file
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-    die("Solo immagini con estensioni JPG, PNG e JPEG sono ammessi.". $imageFileType);
-    $uploadOk = 0;
-    
-}
-
+$fileName = pathinfo($name, PATHINFO_FILENAME);
+$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
 
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
+    $check = getimagesize($tmpName);
+    if($check == false){
         $error = "File is not an image.";
         $uploadOk = 0;
     }
+    $uploadOk = 1;
 }
+
 // Check if file already exists
 if (file_exists($target_file)) {
-    $error = "Sorry, file already exists.";
-    $uploadOk = 0;
+    
+    //Rename the file adding a random number at the end of it
+    $rand = rand(1, 9999);
+    $name = $fileName . $rand . "." . $imageFileType;
+    $target_file = $target_dir . $name;
+    
+    $uploadOk = 1;
 }
+
 // Check file size
-if (1000000 < $_FILES["fileToUpload"]["size"] ) {
+if ($_FILES["fileToUpload"]["size"] > $MAXDIM ) {
     $error = "Sorry, your file is too large.";
     $uploadOk = 0;
 }
 
+//non permette l'Upload di alcuni tipi di file
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+    $error = "Only JPG, PNG and JPEG file admitted. It was a ". $imageFileType;
+    $uploadOk = 0;
+    
+}
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    die("Sorry, your file was not uploaded.");
+    echo "Sorry, your file was not uploaded.";
     }
 // if everything is ok, try to upload file
 else {
-    $caricato = move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
+    $caricato = move_uploaded_file($tmpName, $target_file);
+    $desc = $_POST["descrizione"];
     if($caricato){
-        global $mysqli;
         $user = $_SESSION["utente"];
-        $mysqli->real_escape_string($user);
-        $mysqli -> real_escape_string($name);
-        $query = "INSERT INTO photo (nome, user) VALUES('$name', '$user');";
+        $nameExt = basename($name);
+        $mysqli -> real_escape_string($user);
+        $mysqli -> real_escape_string($nameExt);
+        $query = "INSERT INTO photo (name, user, description) VALUES('$nameExt', '$user','$desc');";
          if(!$mysqli->query($query)){
             die($mysqli->error);
             $error = "error in mysql!";
             }
     }
       else{ 
-        $error = "Sorry, there was an error uploading your file.";
+        $error = "Sorry, there was an error uploading your file. ";
         print_r($_FILES);
       }
 }
 
 $mysqli->close();
-
 session_write_close();
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -91,7 +96,7 @@ session_write_close();
 <body>
 	<h1>Risultati caricamento immagine</h1>
 	<?php if ($error): ?>
-		<p style="color: red"><?php echo $error; print_r($_FILES); ?></p>
+		<p style="color: red"><?php echo $error; print_r($name); ?></p>
 	<?php else: header('Location: /home.php?user='.$_SESSION['utente']);?>	
 	<?php endif ?>
         <?php exit();?>	
