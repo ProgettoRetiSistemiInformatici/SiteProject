daed3319dcd1381f8375cd2567d637c9f36d997d
Gianz91 <gianlucabianchini91@gmail.com>
2017-11-15 20:21:53 +0100

    Update login.php

--- google-login/login.php
+++ google-login/login.php
@@ -16,15 +16,16 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login With Google</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <body>
-    <div class="container" style="margin-top: 100px">
+    <div class="container" style="margin-top: 150px" align="center">
         <div class="row justify-content-center">
             <div class="col-md-6 col-offset-3" align="center">
 
                 <img src="images/photo-machine.png" class="img-thumbnail"width="100"><br><br>
-
+                <h1><b>PHOTOLIO</b></h1>
+                <p><b>A site for photo sharing</b></p>
                 <form action="accesso.php" method="post">
                     <input type="text" name="name" class="form-control" placeholder="Username..."><br>
                     <input type="password" placeholder="Password..." name="password" class="form-control"><br>
