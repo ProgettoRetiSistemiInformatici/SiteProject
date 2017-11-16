7d946e53a8c1c1677387394494fea6ef3ea6fd70
Gianz91 <gianlucabianchini91@gmail.com>
2017-11-16 09:57:54 +0100

    Update changedata.php

--- profiles/changedata.php
+++ profiles/changedata.php
@@ -1,7 +1,5 @@
 <?php
 session_start();
-
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -34,13 +32,15 @@ session_start();
         </header>
         <!--profile current data -->
         <form action="savechanges.php" method="post" enctype="multipart/form-data">
-            <div style="width: 50%; margin: 0 auto;"><div class="profile_img"><img src="<?php echo "profile_images/".$_SESSION['profile']->profile_image; ?>" 
-                                          alt="Immagine "width="75" height="75"> <br>Modifica Immagine del profilo:<input type="file" name="newPimage"><br></div> 
-<div align="center" class="profile_info">Email: <?php echo  $_SESSION['profile']->email ?>| Modifica email personale <input type="text" name="newEmail"><br></div>                                      
-            <div align="center" class="profile_info">Nome: <?php echo  $_SESSION['profile']->firstname ?>| Modifica dati personali <input type="text" name="newName"><br></div>                          
-            <div align="center" class="profile_info">Cognome: <?php echo  $_SESSION['profile']->lastname ?>| Modifica dati personali <input type="text" name="newLName"><br></div>                          
-            <div align="center" class="profile_info">Data di nascita: <?php echo  date('d-m-Y',strtotime($_SESSION['profile']->birth)) ?>|Modifica dati personali<input type="date" name="newBirth"><br></div>                          
-            <div class="profile_info"><textarea name="aboutMe" rows="10" cols="50" maxlength="200" placeholder="Type something about you..."></textarea>
+            <div style="width: 50%; margin: 0 auto;">
+              <div class="profile_img">
+                <img src="<?php echo "profile_images/".$_SESSION['profile']->profile_image; ?>"
+                alt="Immagine "width="75" height="75"> <br>Modifica Immagine del profilo:<input type="file" name="newPimage"><br></div>
+                <div align="center" class="profile_info">Email: <?php echo  $_SESSION['profile']->email ?>| Modifica email personale <input type="text" name="newEmail"><br></div>
+                <div align="center" class="profile_info">Nome: <?php echo  $_SESSION['profile']->firstname ?>| Modifica dati personali <input type="text" name="newName"><br></div>
+                <div align="center" class="profile_info">Cognome: <?php echo  $_SESSION['profile']->lastname ?>| Modifica dati personali <input type="text" name="newLName"><br></div>
+                <div align="center" class="profile_info">Data di nascita: <?php echo  date('d-m-Y',strtotime($_SESSION['profile']->birth)) ?>|Modifica dati personali<input type="date" name="newBirth"><br></div>
+                <div class="profile_info"><textarea name="aboutMe" rows="10" cols="50" maxlength="200" placeholder="Type something about you..."></textarea>
             </div>
             <div align="right" class="profile_info"><input type="submit"></div>
         </form>
