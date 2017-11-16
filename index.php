0a3515b70891917e7c6a99aba5b83406bceda2af
Gianz91 <gianlucabianchini91@gmail.com>
2017-11-16 09:54:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,14 +8,11 @@
         <title>InstaImmagini! - Benvenuto</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     	</head>
-<body> 
+<body>
     <div align ="center"style="margin-top: 100px"><h1>BENVENUTO</h1>
-        <a href="google-login"class="btn btn-primary">Log In</a>|<a href="signup.html">Sign Up</a></p>
+        <a href="google-login"class="btn btn-primary">Log In</a> <i>or</i> <a href="signup.html" class="btn btn-primary">Sign Up</a></p>
 	<a href="admin.html">Admin</a>
     </div>
     <div
 </body>
 </html>
-	
-
-	
