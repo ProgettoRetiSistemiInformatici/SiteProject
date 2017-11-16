a95431c6e590607ac62ce181c981cec76445e1b3
Sby-EasyStuff <manuel.mancini1990@gmail.com>
2017-11-16 12:07:58 +0100

    Update config.php

--- google-login/config.php
+++ google-login/config.php
@@ -2,9 +2,9 @@
 	session_start();
 	require_once "GoogleAPI/vendor/autoload.php";
 	$gClient = new Google_Client();
-	$gClient->setClientId("YOUR_CLIENT_ID");
-	$gClient->setClientSecret("YOUR_CLIENT_SECRET");
-	$gClient->setApplicationName("YOUR_APPLICATION_NAME");
-	$gClient->setRedirectUri("YOUR_REDIRECT_URI");
+	$gClient->setClientId(getenv("GOOGLE_APP_ID");
+	$gClient->setClientSecret(getenv("GOOGLE_APP_SECRET"));
+	$gClient->setApplicationName(getenv("APP_NAME"));
+	$gClient->setRedirectUri(getenv("REDIRECT_URI"));
 	$gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
 ?>
