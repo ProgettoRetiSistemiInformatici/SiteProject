6f2984dd3ec5244619d7bdd0c7c1693b60a9d705
Gianz91 <gianlucabianchini91@gmail.com>
2017-11-16 01:31:22 +0100

    Update createalbum.php

--- gallery/createalbum.php
+++ gallery/createalbum.php
@@ -7,17 +7,14 @@ session_start();
 global $mysqli;
 $ids = $_SESSION['ids'];
 $user= $_SESSION['utente'];
+$cover=$_SESSION['cover'];
 $titolo = $_SESSION['titlealbum'];
-$query="INSERT into album (titolo,user,idFoto) values('$titolo','$user','$ids')";
+$query="INSERT into album (titolo,user,idFoto, cover) values('$titolo','$user','$ids','$cover')";
 if(!$mysqli->query($query)){
     die($mysqli->error);
 }
 header("Location: /profiles/profile.php?user=". $user);
 exit();
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 ?>
