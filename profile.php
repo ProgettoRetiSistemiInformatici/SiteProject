8559eae053ba9dbcce5fafc9b8a69df39115109f
Sby-EasyStuff <manuel.mancini1990@gmail.com>
2017-11-17 09:49:01 +0100

    Add files via upload

--- profile.php
+++ profile.php
@@ -39,13 +39,6 @@ if($date_from_sql != null)
 session_write_close();
 ?>
 
-
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <!DOCTYPE html>
 <html>
 <head>
