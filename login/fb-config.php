411f71c91e6cb6aeb01f23bb03ed635cd5373b17
Sby-EasyStuff <manuel.mancini1990@gmail.com>
2018-02-12 10:30:40 +0100

    Update fb-config.php

--- login/fb-config.php
+++ login/fb-config.php
@@ -4,8 +4,8 @@ require_once 'FB_API/vendor/autoload.php';
 session_start();
 
 $fb = new Facebook\Facebook([
-  'app_id' => '139108946732772', // Replace {app-id} with your app id
-  'app_secret' => '3fa20aec1944b8ea55e28f249caabc6d',
+  'app_id' => 'APP-ID', // Replace {app-id} with your app id
+  'app_secret' => 'APP-SECRET',
   'default_graph_version' => 'v2.10',
   ]);
 
