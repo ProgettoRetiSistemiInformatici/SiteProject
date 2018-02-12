893b776862c8b2827c1bc74be9dbd37f817779c7
Sby-EasyStuff <manuel.mancini1990@gmail.com>
2018-02-12 10:31:27 +0100

    Update config.php

--- login/config.php
+++ login/config.php
@@ -3,8 +3,8 @@
 
 	require_once "GoogleAPI/vendor/autoload.php";
 	$gClient = new Google_Client();
-	$gClient->setClientId("898494140533-eff6egnn3er9sj3fufji274r5ffuondr.apps.googleusercontent.com");
-	$gClient->setClientSecret("iSSuOApVCA-s4Kwq1giIQM5Q");
+	$gClient->setClientId("APP-ID");
+	$gClient->setClientSecret("APP-SECRET");
 	$gClient->setApplicationName("photolio");
 	$gClient->setRedirectUri("http://photolio.altervista.org/login/g-callback.php");
 	$gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
