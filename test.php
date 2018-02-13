463628ae98fe0f202b856fbd16d22742c32357c6
Sby-EasyStuff <manuel.mancini1990@gmail.com>
2018-02-13 14:36:41 +0100

    Delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-  require 'initialization/dbconnection.php';
-  $array = array();
-  $query = "SELECT id FROM photo WHERE id = '1';";
-  if(!$result = $mysqli->query($query)){
-    echo ("errore nella query");
-  }
-  var_dump($result->fetch_object());
-?>
