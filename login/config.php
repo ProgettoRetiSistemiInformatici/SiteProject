cb277134666591a52ea3c3b3600f614762587ab9
Gianz91 <gianlucabianchini91@gmail.com>
2018-02-28 14:59:23 +0100

    Update config.php

--- login/config.php
+++ login/config.php
@@ -3,10 +3,10 @@
 
 	require_once "GoogleAPI/vendor/autoload.php";
 	$gClient = new Google_Client();
-	$gClient->setClientId("940202534728-r2pmss81add0n9c5mpf4q4ip2bsca96n.apps.googleusercontent.com");
-	$gClient->setClientSecret("xGqENpKHcXkP2msAfa1TjDCc");
-	$gClient->setApplicationName("PRSI-Google-Oauth");
-	$gClient->setRedirectUri("http://photolio.com/login/g-callback.php");
+	$gClient->setClientId("your client id");
+	$gClient->setClientSecret("your client secret");
+	$gClient->setApplicationName("your app name");
+	$gClient->setRedirectUri("your redirect to site");
 	$gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
 	$loginURL = $gClient->createAuthUrl();
 
